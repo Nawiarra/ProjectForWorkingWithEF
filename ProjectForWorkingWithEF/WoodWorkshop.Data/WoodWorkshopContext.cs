b635048d74b7cc919d86342cd41800028986ba45
Nawiarra <nika.berezovskaya@ukr.net>
2020-11-10 19:09:29 +0200

    Create context

---
+++ ProjectForWorkingWithEF/WoodWorkshop.Data/WoodWorkshopContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WoodWorkshop.Data.Models;
+
+namespace WoodWorkshop.Data
+{
+    public class WoodWorkshopContext: DbContext
+    {
+        public WoodWorkshopContext() : base("Data Source =(LocalDB)\\MSSQLLocalDB;Initial Catalog = MultiLayerExampleDB;Integrated Security=true")
+        {
+
+        }
+        public WoodWorkshopContext(string connectionString): base(connectionString)
+        {
+
+        }
+
+        DbSet <WoodFurniture> WoodPiecesOfFurniture { get; set; }
+    }
+}
